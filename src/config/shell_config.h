829ca2e19701fb78c55177cde855cd62e1ff2078
leeyangyangy <1787294587@qq.com>
2024-05-28 00:11:35 +0800

    add config

--- src/config/shell_config.h
+++ src/config/shell_config.h
@@ -6,9 +6,58 @@
 * Create         : 2024/5/27 16:41  
 ******************************************************************************  
 */
+#include <wchar.h>
 
 #ifndef MYSHELL_SHELL_CONFIG_H
 #define MYSHELL_SHELL_CONFIG_H
+
+/******************************* readline configuration ****************************/
+
+/* command line input buffer size(byte) */
+#define CONFIG_SHELL_INPUT_BUFFSIZE (127U)
+
+/* set 1 to enable command line edit */
+#define CONFIG_SHELL_LINE_EDITING 1
+
+/* ESC Control Sequence detect, such as Home, Delete, Arrow, etc. */
+#define CONFIG_SHELL_KEY_SEQ_BIND 1
+
+/* set 1 to enable Backslash('\') for line continuation */
+#define CONFIG_SHELL_MULTI_LINE 1
+
+
+/**
+ * set 0 to disable history record.
+ *
+ * nano-shell will take `CONFIG_SHELL_HIST_MIN_RECORD*(2+CONFIG_SHELL_INPUT_BUFFSIZE)` bytes to
+ *   record **at least** `CONFIG_SHELL_HIST_MIN_RECORD` histroys.
+ * the maximum number of history records depends on the average length of the input.
+ */
+#define CONFIG_SHELL_HIST_MIN_RECORD (5U)
+
+
+/******************************* command configuration ****************************/
+
+#define CONFIG_SHELL_FUNC_BRIEF_USAGE 1
+
+#define CONFIG_SHELL_CMD_BRIEF_USAGE  1
+
+#define CONFIG_SHELL_CMD_LONG_HELP 1
+
+/* nano-shell provides a built-in help command, set 0 to disable it */
+#define CONFIG_SHELL_CMD_BUILTIN_HELP 1
+
+/* config the max number of arguments, must be no less than 1. */
+#define CONFIG_SHELL_CMD_MAX_ARGC (10U)
+
+
+/******************************* shell io configuration ****************************/
+
+/* config the buffer size (shell_printf()) */
+#define CONFIG_SHELL_PRINTF_BUFFER_SIZE 128U
+
+/*******************************  shell configuration  ****************************/
+#define CONFIG_SHELL_PROMPT "⚡leeyangy-shell😀>> "
 
 
 #endif //MYSHELL_SHELL_CONFIG_H
--- src/constant/init.h
+++ src/constant/init.h
@@ -10,16 +10,21 @@
 #ifndef MYSHELL_INIT_H
 #define MYSHELL_INIT_H
 
-// get system date time ,use __DATE__ __TIME__
+// shell version
+#define MY_SHELL_BUILD_VERDION "1.0"
 
+// get system date time ,use __DATE__ __TIME__
 char* init_info=" _      ______ ________     __      _   _  _______     __   _____ _    _ ______ _      _       __      ____   ___  \n"
                 "| |    |  ____|  ____\\ \\   / //\\   | \\ | |/ ____\\ \\   / /  / ____| |  | |  ____| |    | |      \\ \\    / /_ | / _ \\ \n"
                 "| |    | |__  | |__   \\ \\_/ //  \\  |  \\| | |  __ \\ \\_/ /  | (___ | |__| | |__  | |    | |       \\ \\  / / | || | | |\n"
                 "| |    |  __| |  __|   \\   // /\\ \\ | . ` | | |_ | \\   /    \\___ \\|  __  |  __| | |    | |        \\ \\/ /  | || | | |\n"
                 "| |____| |____| |____   | |/ ____ \\| |\\  | |__| |  | |     ____) | |  | | |____| |____| |____     \\  /   | || |_| |\n"
                 "|______|______|______|  |_/_/    \\_\\_| \\_|\\_____|  |_|    |_____/|_|  |_|______|______|______|     \\/    |_(_)___/ \n"
-                "build time: " __DATE__ " " __TIME__ "\n"
-                "please input help to get help info\n";
+                "Welcome to the leeyangy-shell " MY_SHELL_BUILD_VERDION "\r\n"
+                "Build time: " __DATE__ " " __TIME__ "\n"
+                "Source code: https://github.com/leeyangyangy/leeyangy-shell\n"
+                "More new features are being developed.\n"
+                "Please input help to get help info.\n";
 
 
 
